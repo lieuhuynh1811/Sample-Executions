a0747c3e4eb03fa6fa1c7ff050c19d2fd8dd26e8
Lieu Huynh <lieu.huynh@Lieus-MacBook-Pro.local>
2025-08-29 12:59:28 +0700

    update

--- Test Suites/healthcare-tests - TS_RegressionTest-29082025 - updated.ts
+++ Test Suites/healthcare-tests - TS_RegressionTest-29082025 - updated.ts
@@ -1,7 +1,7 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <TestSuiteEntity>
    <description></description>
-   <name>healthcare-tests - TS_RegressionTest-29082025</name>
+   <name>healthcare-tests - TS_RegressionTest-29082025 - updated</name>
    <tag></tag>
    <isRerun>false</isRerun>
    <mailRecipient></mailRecipient>
