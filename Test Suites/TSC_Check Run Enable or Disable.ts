9849cfb9c5d335b6733301e156c0c9a40e5de93e
Lieu Huynh <lieu.huynh@Lieus-MacBook-Pro.local>
2025-08-29 14:25:45 +0700

    update

--- Test Suites/TSC_Check Run Enable or Disable.ts
+++ Test Suites/TSC_Check Run Enable or Disable.ts
@@ -11,29 +11,8 @@
          <configuration>
             <groupName>Web Desktop</groupName>
             <profileName>default</profileName>
-            <requireConfigurationData>true</requireConfigurationData>
-            <runConfigurationData>
-               <entry>
-                  <key>testCloudDesktopBrowser</key>
-                  <value>{
-  &quot;id&quot;: 735,
-  &quot;osDisplayName&quot;: &quot;MacOS&quot;,
-  &quot;browserName&quot;: &quot;firefox&quot;,
-  &quot;browserDisplayName&quot;: &quot;Firefox&quot;,
-  &quot;browserVersion&quot;: &quot;141&quot;,
-  &quot;os&quot;: &quot;macos&quot;,
-  &quot;platformName&quot;: &quot;IOS&quot;,
-  &quot;timeout&quot;: 15,
-  &quot;usingTunnel&quot;: false,
-  &quot;isHighAvailableDevice&quot;: false
-}</value>
-               </entry>
-               <entry>
-                  <key>testCloudSessionType</key>
-                  <value>DESKTOP_BROWSER</value>
-               </entry>
-            </runConfigurationData>
-            <runConfigurationId>TestCloud</runConfigurationId>
+            <requireConfigurationData>false</requireConfigurationData>
+            <runConfigurationId>Chrome</runConfigurationId>
          </configuration>
          <runEnabled>true</runEnabled>
          <testSuiteEntity>Test Suites/TS Login</testSuiteEntity>
@@ -47,12 +26,12 @@
                <entry>
                   <key>testCloudDesktopBrowser</key>
                   <value>{
-  &quot;id&quot;: 448,
-  &quot;osDisplayName&quot;: &quot;MacOS&quot;,
+  &quot;id&quot;: 892,
+  &quot;osDisplayName&quot;: &quot;Windows&quot;,
   &quot;browserName&quot;: &quot;msedge&quot;,
   &quot;browserDisplayName&quot;: &quot;Edge Chromium&quot;,
-  &quot;browserVersion&quot;: &quot;132&quot;,
-  &quot;os&quot;: &quot;macos&quot;,
+  &quot;browserVersion&quot;: &quot;141&quot;,
+  &quot;os&quot;: &quot;windows&quot;,
   &quot;platformName&quot;: &quot;IOS&quot;,
   &quot;timeout&quot;: 15,
   &quot;usingTunnel&quot;: false,
@@ -72,30 +51,9 @@
       <TestSuiteRunConfiguration>
          <configuration>
             <groupName>Web Desktop</groupName>
-            <profileName>default</profileName>
-            <requireConfigurationData>true</requireConfigurationData>
-            <runConfigurationData>
-               <entry>
-                  <key>testCloudDesktopBrowser</key>
-                  <value>{
-  &quot;id&quot;: 732,
-  &quot;osDisplayName&quot;: &quot;Linux&quot;,
-  &quot;browserName&quot;: &quot;chrome&quot;,
-  &quot;browserDisplayName&quot;: &quot;Chrome&quot;,
-  &quot;browserVersion&quot;: &quot;137&quot;,
-  &quot;os&quot;: &quot;linux&quot;,
-  &quot;platformName&quot;: &quot;IOS&quot;,
-  &quot;timeout&quot;: 15,
-  &quot;usingTunnel&quot;: false,
-  &quot;isHighAvailableDevice&quot;: false
-}</value>
-               </entry>
-               <entry>
-                  <key>testCloudSessionType</key>
-                  <value>DESKTOP_BROWSER</value>
-               </entry>
-            </runConfigurationData>
-            <runConfigurationId>TestCloud</runConfigurationId>
+            <profileName>QA</profileName>
+            <requireConfigurationData>false</requireConfigurationData>
+            <runConfigurationId>Chrome (headless)</runConfigurationId>
          </configuration>
          <runEnabled>true</runEnabled>
          <testSuiteEntity>Test Suites/TS Home Page</testSuiteEntity>
@@ -109,12 +67,12 @@
                <entry>
                   <key>testCloudDesktopBrowser</key>
                   <value>{
-  &quot;id&quot;: 105,
-  &quot;osDisplayName&quot;: &quot;MacOS&quot;,
-  &quot;browserName&quot;: &quot;safari&quot;,
-  &quot;browserDisplayName&quot;: &quot;Safari&quot;,
-  &quot;browserVersion&quot;: &quot;14&quot;,
-  &quot;os&quot;: &quot;macos&quot;,
+  &quot;id&quot;: 730,
+  &quot;osDisplayName&quot;: &quot;Linux&quot;,
+  &quot;browserName&quot;: &quot;chrome&quot;,
+  &quot;browserDisplayName&quot;: &quot;Chrome&quot;,
+  &quot;browserVersion&quot;: &quot;136&quot;,
+  &quot;os&quot;: &quot;linux&quot;,
   &quot;platformName&quot;: &quot;IOS&quot;,
   &quot;timeout&quot;: 15,
   &quot;usingTunnel&quot;: false,
